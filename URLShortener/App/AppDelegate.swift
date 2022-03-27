8cf09ccbe7e1528acf0145f03740935a99ff3445
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-03-27 23:17:32 +0200

    Updated SceneDelegate

--- URLShortener/App/AppDelegate.swift
+++ URLShortener/App/AppDelegate.swift
@@ -8,32 +8,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
   typealias LaunchOptions = [UIApplication.LaunchOptionsKey: Any]?
 
-  // MARK: - Properties
-
-  lazy var persistentContainer: NSPersistentCloudKitContainer = {
-    let container = NSPersistentCloudKitContainer(name: "URLShortener")
-    container.loadPersistentStores { _, error in
-      if let error = error as NSError? {
-        fatalError("Unresolved error \(error), \(error.userInfo)")
-      }
-    }
-    return container
-  }()
-
-  // MARK: - Public Methods
-
-  func saveContext() {
-    let context = persistentContainer.viewContext
-    if context.hasChanges {
-      do {
-        try context.save()
-      } catch {
-        let nserror = error as NSError
-        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-      }
-    }
-  }
-
   // MARK: - UIApplicationDelegate
 
   func application(_ application: UIApplication,
