92b6780f90b93eb3ac28cf6d44abcf064014a4e5
Dmitrii Kuznetsov <dsk1306@icloud.com>
2022-04-26 21:09:52 +0300

    Updated insets in LinksHistoryEmptyView (#9)

--- URLShortener/UI/LinksHistory/-Views/LinksHistoryEmptyView.swift
+++ URLShortener/UI/LinksHistory/-Views/LinksHistoryEmptyView.swift
@@ -39,9 +39,9 @@ extension LinksHistory {
       backgroundImageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
       backgroundImageView.setContentHuggingPriority(.defaultLow, for: .vertical)
       backgroundImageView.add(to: self) {
-        $0.topAnchor.constraint(equalTo: $1.topAnchor, constant: Constant.verticalInset)
-        $0.leadingAnchor.constraint(equalTo: $1.leadingAnchor, constant: Constant.horizontalInset)
-        $1.trailingAnchor.constraint(equalTo: $0.trailingAnchor, constant: Constant.horizontalInset)
+        $0.topAnchor.constraint(equalTo: $1.topAnchor, constant: Constant.inset)
+        $0.leadingAnchor.constraint(equalTo: $1.leadingAnchor, constant: Constant.inset)
+        $1.trailingAnchor.constraint(equalTo: $0.trailingAnchor, constant: Constant.inset)
       }
 
       // Text stack view.
@@ -53,10 +53,10 @@ extension LinksHistory {
       }
 
       textStackView.add(to: self) {
-        $0.leadingAnchor.constraint(equalTo: $1.leadingAnchor, constant: Constant.horizontalInset)
-        $1.trailingAnchor.constraint(equalTo: $0.trailingAnchor, constant: Constant.horizontalInset)
-        $0.topAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: Constant.verticalInset)
-        $1.bottomAnchor.constraint(equalTo: $0.bottomAnchor, constant: Constant.verticalInset)
+        $0.leadingAnchor.constraint(equalTo: $1.leadingAnchor, constant: Constant.inset)
+        $1.trailingAnchor.constraint(equalTo: $0.trailingAnchor, constant: Constant.inset)
+        $0.topAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: Constant.inset)
+        $1.bottomAnchor.constraint(equalTo: $0.bottomAnchor, constant: Constant.inset)
       }
 
       // Title label.
@@ -80,8 +80,7 @@ private extension LinksHistory.EmptyView {
 
   enum Constant {
 
-    static let horizontalInset: CGFloat = 24
-    static let verticalInset: CGFloat = 12
+    static let inset: CGFloat = 32
 
   }
 
