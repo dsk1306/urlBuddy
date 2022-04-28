b0d0fe218a5121d606763e6dc62bdd158aba00a8
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-04-28 13:40:15 +0300

    Updated ClipboardService

--- URLShortener/Services/Clipboard/DefaultClipboardService.swift
+++ URLShortener/Services/Clipboard/DefaultClipboardService.swift
@@ -6,6 +6,10 @@ final class DefaultClipboardService {}
 
 extension DefaultClipboardService: ClipboardService {
 
+  var string: String? {
+    UIPasteboard.general.string
+  }
+
   func paste(link: Link) {
     UIPasteboard.general.string = link.shortenString
   }
