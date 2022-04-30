d88a55790b2e821eca33dc31a6ce5862f37586bd
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-05-01 01:23:51 +0300

    Removed Publisher extensions

--- URLShortener/Extensions/Publisher+Ext.swift
+++ URLShortener/Extensions/Publisher+Ext.swift
@@ -3,11 +3,6 @@ import Foundation
 
 extension Publisher {
 
-  /// Erases publisher and send it output on main queue.
-  func prepareToOutput() -> AnyPublisher<Output, Failure> {
-    self.receive(on: DispatchQueue.main).eraseToAnyPublisher()
-  }
-
   func `catch`(errorHandler: @escaping (Error) -> Void) -> AnyPublisher<Output, Never> {
     // swiftlint:disable:next trailing_closure
     map { value -> Output? in value }
